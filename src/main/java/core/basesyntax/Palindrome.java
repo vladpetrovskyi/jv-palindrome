35a7554a789ea4aeda11c522abfc3a7a4062e7a7
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-01 14:13:58 +0300

    added solution for hw 3.3

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -9,18 +9,17 @@ public class Palindrome {
      * При определении "палиндромности" строки должны учитываться только буквы и цифры. А пробелы,
      * знаки препинания, а также регистр символов должны игнорироваться.</p>
      *
-     * <p>Подсказки (не читайте, если хотите решить сами):
-     * для удаления из строки всех символов, не являющихся буквами и цифрами, можно воспользоваться
-     * регулярным выражением. Найдите в классе String метод, выполняющий замену по
-     * регулярному выражению;
-     * для перестановки символов строки в обратном порядке можно воспользоваться методом reverse(),
-     * который находится в классе StringBuilder;
-     * в классе String есть методы для преобразования всей строки в верхний и нижний регистр.</p>
-     *
      * <p>Пример: Madam, I'm Adam!</p>
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String noSymbols = text.toLowerCase().replaceAll("[^a-z[0-9]]", "");
+        StringBuilder forward = new StringBuilder();
+        StringBuilder reverse = new StringBuilder();
+        for (int i = 0; i < noSymbols.length(); i++) {
+            forward.append(noSymbols.charAt(i));
+            reverse.append(noSymbols.charAt(noSymbols.length() - 1 - i));
+        }
+        return forward.toString().equals(reverse.toString());
     }
 }
